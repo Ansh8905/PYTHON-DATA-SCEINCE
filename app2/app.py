2bc69c09632a65290d0c91ac72a99d77a50244f0
Ansh8905 <109736202+Ansh8905@users.noreply.github.com>
2022-09-27 18:19:09 +0530

    Update app.py

--- app2/app.py
+++ app2/app.py
@@ -1,3 +1,4 @@
+from pyparsing import col
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -30,3 +31,28 @@ st.sidebar.header("OPTIONS")
 view_data = st.sidebar.checkbox("see raw data")
 if view_data:
     st.write(df)
+
+numerical_cols = df.select_dtypes(include=np.number).columns.tolist()
+pd.Categorical_cols = df.select_dtypes(include='object').columns.tolist()
+#st.write(numerical_cols)
+#st.write(pd.Categorical_cols)
+colx = st.sidebar.selectbox("select a numerical column x",numerical_cols)
+coly = st.sidebar.selectbox("select a numerical column y",numerical_cols)
+
+graph_types = ['line','area','bar','scatter']
+gtype = st.sidebar.radio("select a grapg type", graph_types)
+
+if gtype == graph_types[0]:
+    fig = px.line(df, x=colx, y=coly)
+if gtype == graph_types[1]:
+    fig = px.area(df, x=colx, y=coly)
+
+if gtype == graph_types[2]:
+    fig = px.bar(df, x=colx, y=coly)
+
+
+if gtype == graph_types[3]:
+    fig = px.scatter(df, x=colx, y=coly)
+
+if fig:
+    st.plotly_chart(fig, use_container_width=True)
